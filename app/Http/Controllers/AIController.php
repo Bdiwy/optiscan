4ca723cef66d6f5bc1da75075f153fa8413b6119
Ahmed Bdiwy <ahmedbedawi22@gmail.com>
2024-01-17 03:38:40 +0200

    handle endpoint

--- app/Http/Controllers/AIController.php
+++ app/Http/Controllers/AIController.php
@@ -2,33 +2,20 @@
 // AIController.php
 
 namespace App\Http\Controllers;
+use App\Models\user_disease_pivot;
 use Illuminate\Http\Request;
-use Phpml\Classification\SVC;
-use Phpml\SupportVectorMachine\Kernel;
+use TensorFlow\TensorFlow;
 
 class AIController extends Controller
 {
 
-    private $classifier;
-
-    public function __construct()
-    {
-        // Load your pre-trained model during controller initialization
-        $this->classifier = new SVC(Kernel::LINEAR);
-        $this->classifier->load('public/Aimodel/efficientnetb3-EyeDisease-96.22.tflite');
-        // You would load your trained model here. This is just a placeholder.
-        // Example: $this->classifier->load('path/to/your/pretrained-model');
-    }
-
     public function predict(Request $request)
     {
-        // Get input data from the request
-        $inputData = $request->input('data');
-
-        // Make predictions using the loaded model
-        $predictions = $this->classifier->predict([$inputData]);
-
-        // Return the predictions as a JSON response
-        return response()->json(['predictions' => $predictions]);
+        
+        user_disease_pivot::create([
+        
+        'user_id'=>'1',
+            'diseases'=>  $request->result
+        ]);
     }
 }
